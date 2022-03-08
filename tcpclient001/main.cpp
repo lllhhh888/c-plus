e0edcb8d43b91e96b9b858e23c00c792a4b32222
lh <lihuilihuiwork@gmail.com>
2022-03-08 19:53:11 +0800

    1.02版本，定义报文头，长度

--- tcpclient001/main.cpp
+++ tcpclient001/main.cpp
@@ -4,11 +4,64 @@
 
 using namespace std;
 
+enum {
+	CMD_LOGIN,
+	CMD_LOGOUT
+};
+
+typedef struct _DataHeader {
+	short dataLength;
+	short cmd;
+}DataHeader;
+
+typedef struct _Login : public _DataHeader {
+	char username[15];
+	char password[15];
+	_Login() {
+		cmd = CMD_LOGIN;
+		dataLength = sizeof(_Login);
+	}
+}Login;
+
+typedef struct _DataBody {
+	short code;
+	char msg[20];
+}DataBody;
+
+typedef struct _LogOut : public _DataHeader {
+	char username[15];
+	_LogOut() {
+		cmd = CMD_LOGOUT;
+		dataLength = sizeof(_LogOut);
+	}
+}LogOut;
+
 typedef struct _InfoData
 {
 	int age;
 	char name[5];
 }InfoData;
+
+
+template <typename T> bool parseBody (SOCKET sock, T *body) {
+
+	char headerChar[50] = {};
+
+	int headerSize = sizeof(DataHeader);
+
+	int recvHeadLen = recv(sock, headerChar, headerSize, 0);
+
+	if (recvHeadLen <= 0) return false;
+
+	DataHeader*  header = (DataHeader*)headerChar;
+
+	int bodyLen = recv(sock, (char*)body, header->dataLength - headerSize, 0);
+
+	if (bodyLen <= 0) return false;
+
+	return true;
+}
+
 
 int main() {
 	WORD version = MAKEWORD(2, 2);
@@ -22,6 +75,8 @@ int main() {
 		return 0;
 	}
 
+
+
 	sockaddr_in _sin;
 	_sin.sin_family = AF_INET;
 	_sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
@@ -33,32 +88,100 @@ int main() {
 		return 0;
 	}
 
-	char sendMsg[50] = {};
+	char cmd[50] = {};
 	while (true)
 	{
 
-		InfoData infoData;
 
-		memset(sendMsg, 0, sizeof(sendMsg));
+		char* msg = NULL;
+		DataBody result;
+
+		memset(cmd, 0, sizeof(cmd));
+
 		cout << "请输入你需要发送的信息: " << endl;
 
-		scanf("%s", sendMsg);
+		scanf("%s", cmd);
+
+		if (0 == strcmp("login", cmd)) {
+			Login login;
+
+			cout << "请输入用户名" << endl;
+
+			scanf("%s", login.username);
+
+			cout << "请输入密码" << endl;
+
+			scanf("%s", login.password);
+
+			send(client, (char*)&login, sizeof(login), 0);
 
 
-		if (SOCKET_ERROR == send(client, sendMsg, strlen(sendMsg) + 1, 0)) {
-			cout << "发送失败" << endl;
+			int parseRes = parseBody(client, &result);
+
+			if (!parseRes) break;
+
+			cout << "接受到参数code:" << result.code << ",msg:" << result.msg << ";" << endl;
+
+			if (1 == result.code) {
+				cout << "登录成功" << endl;
+			}
+			else {
+				cout << "登录失败" << endl;
+			}
+
+
+		}
+		else if (0 == strcmp("logout", cmd)) {
+			LogOut  logout;
+
+
+			cout << "请输入登出的账户" << endl;
+
+			scanf("%s", logout.username);
+
+			send(client, (char*)&logout, sizeof(logout), 0);
+
+			int parseRes = parseBody(client, &result);
+
+			if (!parseRes) break;
+
+			cout << "接受到参数code:" << result.code << ",msg:" << result.msg << ";" << endl;
+
+			if (1 == result.code) {
+				cout << "登出成功" << endl;
+			}
+			else {
+				cout << "登出失败" << endl;
+			}
+
+
+
+
+			//dataHeader.cmd = CMD_LOGOUT;
+		}
+		else {
+			cout << "命令不存在，请重新输入" << endl;
 			continue;
 		}
 
 
-		if (recv(client, (char*)&infoData, sizeof(infoData), 0) > 0) {
-			
-			cout << infoData.age << endl;
-			cout << infoData.name << endl;
-		}
-		else {
-			cout << "接受失败" << endl;
-		}
+		/*send(client, (char*)&dataHeader, sizeof(dataHeader), 0);
+		send(client, msg, dataHeader.dataLength + 1, 0);*/
+		//if (SOCKET_ERROR == send(client, cmd, strlen(cmd) + 1, 0)) {
+		//	cout << "发送失败" << endl;
+		//	continue;
+		//}
+
+
+		//if (recv(client, (char*)&infoData, sizeof(infoData), 0) > 0) {
+
+		//	cout << infoData.age << endl;
+		//	cout << infoData.name << endl;
+		//}
+		//else {
+		//	cout << "接受失败" << endl;
+		//}
+
 	}
 
 
--- tcpserver001/main.cpp
+++ tcpserver001/main.cpp
@@ -5,11 +5,52 @@
 
 using namespace std;
 
-typedef struct _InfoData
-{
-	int age;
-	char name[5];
-}InfoData;
+
+
+enum {
+	CMD_LOGIN,
+	CMD_LOGOUT
+};
+
+typedef struct _DataHeader {
+	short dataLength;
+	short cmd;
+}DataHeader;
+
+typedef struct _Login {
+	char username[15];
+	char password[15];
+}Login;
+
+typedef struct _LogOut {
+	char username[15];
+}LogOut;
+
+typedef struct _LoginResult : public DataHeader {
+	short code;
+	char msg[20];
+	_LoginResult() {
+		cmd = CMD_LOGIN;
+		dataLength = sizeof(_LoginResult);
+	}
+}LoginResult;
+
+
+typedef struct _DataBoby {
+	short code;
+	char msg[20];
+}DataBody;
+
+
+
+typedef struct _LogOutResult : public DataHeader, public _DataBoby {
+	_LogOutResult() {
+		cmd = CMD_LOGIN;
+		dataLength = sizeof(_LoginResult);
+	}
+}LogOutResult;
+
+
 
 int main() {
 	WORD version = MAKEWORD(2, 2);
@@ -37,7 +78,7 @@ int main() {
 	listen(server, 20);
 	cout << "启动服务成功，监听5555端口中" << endl;
 
-   
+
 	char recvMsg[50] = {};
 	int sendRes;
 	while (true)
@@ -50,39 +91,93 @@ int main() {
 			continue;
 		}
 
-		while (true) {
-			memset(recvMsg, 0, sizeof(recvMsg));
-			InfoData infoData;
-			int ret = recv(client, recvMsg, sizeof(recvMsg), 0);
-			if (ret <= 0) break;
 
-			cout << "接收到命令" << recvMsg << endl;
+		while (true)
+		{
+
+
+			char headerChar[1024] = {};
+			int headerSize = sizeof(DataHeader);
+
+			int recvLen = recv(client, headerChar, headerSize, 0);
+
+			DataHeader* header = (DataHeader*)headerChar;
+
+			cout << header->cmd << endl;
+
+			cout << header->dataLength << endl;
+
+			int len = 1;
+			Login login;
+			LogOut out;
+			LoginResult result;
+			LogOutResult log_out_result;
+			switch (header->cmd)
+			{
+
+			case CMD_LOGIN:
+				len = recv(client, (char*)&login, header->dataLength - headerSize, 0);
+				if (len <= 0) break;
+				cout << login.username << endl;
+				cout << login.password << endl;
+				if (0 == strcmp(login.username, "lh") && 0 == strcmp(login.password, "123456")) {
+					result.code = 1;
+					strcpy(result.msg, "成功");
+				}
+				else {
+					result.code = 0;
+					strcpy(result.msg, "失败");
+				}
+				send(client, (char*)&result, sizeof(result), 0);
+				break;
+			case CMD_LOGOUT:
+				len = recv(client, (char*)&out, header->dataLength - headerSize, 0);
+				if (len <= 0) break;
+				cout << out.username << endl;
+				if (0 == strcmp(out.username, "lh")) {
+					log_out_result.code = 1;
+					strcpy(log_out_result.msg, "登出成功");
+				}
+				else {
+					log_out_result.code = 0;
+					strcpy(log_out_result.msg, "没有该账户");
+				}
+				send(client, (char*)&log_out_result, sizeof(log_out_result), 0);
+				break;
+
+			default:
+				cout << "进来1" << endl;
+				break;
+			}
+
+
 			
 
-			if (strcmp("getInfo", recvMsg) == 0) {
-				infoData.age = 18;
-				strcpy(infoData.name, "lh");
-				
-				
-				sendRes = send(client,(char*)&infoData, sizeof(infoData), 0);
-			}
-			else if (strcmp("getNum", recvMsg) == 0) {
-			    
-				sendRes = send(client, "222", 3, 0);
-			}
-			else {
-			
-				sendRes = send(client, "???", 3, 0);
-			}
+			if (len <= 0) break;
+		}
 
-			if (SOCKET_ERROR == sendRes) {
+		cout << "结束会话" << endl;
+
+		//switch (dataHeader.cmd)
+		//{
+		//CMD_LOGIN:
+		//	break;
+		//CMD_LOGOUT:
+		//	break;
+		//default:
+		//	break;
+		//}
+
+
+
+
+
+		/*	if (SOCKET_ERROR == sendRes) {
 				cout << "发送失败" << endl;
 			}
 			else {
 				cout << "发送成功" << endl;
-			}
-		}
-		cout << "end" << endl;
+			}*/
 
 	}
 
