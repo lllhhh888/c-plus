772ee39c76ec0624af91b8618d4e40d3b693ac25
lh <lihuilihuiwork@gmail.com>
2022-03-09 20:06:10 +0800

    删除多余的文件

--- tcpserver001/main.cpp
+++ tcpserver001/main.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -51,6 +52,68 @@ typedef struct _LogOutResult : public DataHeader, public _DataBoby {
 }LogOutResult;
 
 
+bool processTasks(SOCKET client) {
+
+	char headerChar[1024] = {};
+	int headerSize = sizeof(DataHeader);
+
+	int recvLen = recv(client, headerChar, headerSize, 0);
+
+	DataHeader* header = (DataHeader*)headerChar;
+
+	cout << header->cmd << endl;
+
+	cout << header->dataLength << endl;
+
+	int len = 1;
+	Login login;
+	LogOut out;
+	LoginResult result;
+	LogOutResult log_out_result;
+	switch (header->cmd)
+	{
+
+	case CMD_LOGIN:
+		len = recv(client, (char*)&login, header->dataLength - headerSize, 0);
+		if (len <= 0) break;
+		cout << login.username << endl;
+		cout << login.password << endl;
+		if (0 == strcmp(login.username, "lh") && 0 == strcmp(login.password, "123456")) {
+			result.code = 1;
+			strcpy(result.msg, "成功");
+		}
+		else {
+			result.code = 0;
+			strcpy(result.msg, "失败");
+		}
+		send(client, (char*)&result, sizeof(result), 0);
+		break;
+	case CMD_LOGOUT:
+		len = recv(client, (char*)&out, header->dataLength - headerSize, 0);
+		if (len <= 0) break;
+		cout << out.username << endl;
+		if (0 == strcmp(out.username, "lh")) {
+			log_out_result.code = 1;
+			strcpy(log_out_result.msg, "登出成功");
+		}
+		else {
+			log_out_result.code = 0;
+			strcpy(log_out_result.msg, "没有该账户");
+		}
+		send(client, (char*)&log_out_result, sizeof(log_out_result), 0);
+		break;
+
+	default:
+		cout << "进来1" << endl;
+		break;
+	}
+
+	return len <= 0;
+
+
+}
+
+vector<SOCKET> g_clients;
 
 int main() {
 	WORD version = MAKEWORD(2, 2);
@@ -59,8 +122,8 @@ int main() {
 
 
 
-	SOCKET server = socket(AF_INET, SOCK_STREAM, 0);
-	if (INVALID_SOCKET == server) {
+	SOCKET _sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (INVALID_SOCKET == _sock) {
 		cout << "创建socket失败" << endl;
 		return 0;
 	}
@@ -70,121 +133,99 @@ int main() {
 	_sin.sin_addr.S_un.S_addr = INADDR_ANY;
 	_sin.sin_port = htons(5555);
 
-	if (SOCKET_ERROR == bind(server, (sockaddr*)&_sin, sizeof(_sin))) {
+	if (SOCKET_ERROR == bind(_sock, (sockaddr*)&_sin, sizeof(_sin))) {
 		cout << "绑定失败" << endl;
 		return 0;
 	}
 
-	listen(server, 20);
+	listen(_sock, 20);
 	cout << "启动服务成功，监听5555端口中" << endl;
 
 
 	char recvMsg[50] = {};
 	int sendRes;
+
+
 	while (true)
 	{
-		sockaddr_in clientAddr;
-		int clientAddrSize = sizeof(clientAddr);
-		SOCKET client = accept(server, (sockaddr*)&clientAddr, &clientAddrSize);
-		if (INVALID_SOCKET == client) {
-			cout << "连接失败" << endl;
-			continue;
-		}
+		//伯克利 socket
+		fd_set fdRead;
+		fd_set fdWrite;
+		fd_set fdExp;
 
+		FD_ZERO(&fdRead);
+		FD_ZERO(&fdWrite);
+		FD_ZERO(&fdExp);
 
-		while (true)
+		FD_SET(_sock, &fdRead);
+		FD_SET(_sock, &fdWrite);
+		FD_SET(_sock, &fdExp);
+
+		for (int n = (int)g_clients.size() - 1; n >= 0; n--)
 		{
-
-
-			char headerChar[1024] = {};
-			int headerSize = sizeof(DataHeader);
-
-			int recvLen = recv(client, headerChar, headerSize, 0);
-
-			DataHeader* header = (DataHeader*)headerChar;
-
-			cout << header->cmd << endl;
-
-			cout << header->dataLength << endl;
-
-			int len = 1;
-			Login login;
-			LogOut out;
-			LoginResult result;
-			LogOutResult log_out_result;
-			switch (header->cmd)
+			FD_SET(g_clients[n], &fdRead);
+		}
+		///nfds 是一个整数值 是指fd_set集合中所有描述符(socket)的范围，而不是数量
+		///既是所有文件描述符最大值+1 在Windows中这个参数可以写0
+		//timeval t = { 0,0 };
+		int ret = select(_sock + 1, &fdRead, &fdWrite, &fdExp, NULL);
+		if (ret < 0)
+		{
+			printf("select任务结束。\n");
+			break;
+		}
+		if (FD_ISSET(_sock, &fdRead))
+		{
+			FD_CLR(_sock, &fdRead);
+			// 4 accept 等待接受客户端连接
+			sockaddr_in clientAddr = {};
+			int nAddrLen = sizeof(sockaddr_in);
+			SOCKET _cSock = INVALID_SOCKET;
+			_cSock = accept(_sock, (sockaddr*)&clientAddr, &nAddrLen);
+			if (INVALID_SOCKET == _cSock)
 			{
-
-			case CMD_LOGIN:
-				len = recv(client, (char*)&login, header->dataLength - headerSize, 0);
-				if (len <= 0) break;
-				cout << login.username << endl;
-				cout << login.password << endl;
-				if (0 == strcmp(login.username, "lh") && 0 == strcmp(login.password, "123456")) {
-					result.code = 1;
-					strcpy(result.msg, "成功");
-				}
-				else {
-					result.code = 0;
-					strcpy(result.msg, "失败");
-				}
-				send(client, (char*)&result, sizeof(result), 0);
-				break;
-			case CMD_LOGOUT:
-				len = recv(client, (char*)&out, header->dataLength - headerSize, 0);
-				if (len <= 0) break;
-				cout << out.username << endl;
-				if (0 == strcmp(out.username, "lh")) {
-					log_out_result.code = 1;
-					strcpy(log_out_result.msg, "登出成功");
-				}
-				else {
-					log_out_result.code = 0;
-					strcpy(log_out_result.msg, "没有该账户");
-				}
-				send(client, (char*)&log_out_result, sizeof(log_out_result), 0);
-				break;
-
-			default:
-				cout << "进来1" << endl;
-				break;
+				printf("错误,接受到无效客户端SOCKET...\n");
 			}
-
-
-			
-
-			if (len <= 0) break;
+			g_clients.push_back(_cSock);
+			printf("新客户端加入：socket = %d,IP = %s \n", (int)_cSock, inet_ntoa(clientAddr.sin_addr));
 		}
 
-		cout << "结束会话" << endl;
 
-		//switch (dataHeader.cmd)
-		//{
-		//CMD_LOGIN:
-		//	break;
-		//CMD_LOGOUT:
-		//	break;
-		//default:
-		//	break;
-		//}
+		cout << fdRead.fd_count << endl;
+		if (processTasks(fdRead.fd_array[0]))
+		{
+			auto iter = find(g_clients.begin(), g_clients.end(), fdRead.fd_array[0]);
+			if (iter != g_clients.end())
+			{
+				g_clients.erase(iter);
+				closesocket(fdRead.fd_array[0]);
+				cout << "colse link" << endl;
 
-
-
-
-
-		/*	if (SOCKET_ERROR == sendRes) {
-				cout << "发送失败" << endl;
 			}
-			else {
-				cout << "发送成功" << endl;
-			}*/
+		}
 
+
+		/*	for (size_t n = 0; n < fdRead.fd_count; n++)
+			{
+				if (processTasks(fdRead.fd_array[n]))
+				{
+					auto iter = find(g_clients.begin(), g_clients.end(), fdRead.fd_array[n]);
+					if (iter != g_clients.end())
+					{
+						g_clients.erase(iter);
+						closesocket(fdRead.fd_array[n]);
+
+					}
+				}
+			}*/
 	}
 
 
 
+
+
 	WSACleanup();
-	closesocket(server);
+	closesocket(_sock);
 	system("pause");
 	return 0;
 }
--- test001/main.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-int main() {
-	std::cout << "test001" << std::endl;
-}
